328ec07a9534c1ed169ed1be35b25fc88c880a92
ShallowGreen <duanguangxin2022@gmail.com>
2023-09-03 16:52:11 +0800

    添加ssd1306显示效果

--- example/esp-idf-ssd1306-master/TextDemo/main/main.c
+++ example/esp-idf-ssd1306-master/TextDemo/main/main.c
@@ -1,4 +1,9 @@
-#include "user.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
 
 #include "ssd1306.h"
 #include "font8x8_basic.h"
@@ -21,7 +26,7 @@
 
 #define tag "SSD1306"
 
-void SSD1306_main(void)
+void app_main(void)
 {
 	SSD1306_t dev;
 	int center, top, bottom;
@@ -193,37 +198,4 @@ void SSD1306_main(void)
 
 	// Restart module
 	esp_restart();
-}
-
-
-// #define LED_PIN      2
-// #define LED_PIN_SEL (1UL << LED_PIN)
-
-void user_task(void *pvParameter)
-{
-    SSD1306_main();
-//     // zero-initialize the config structure.
-//     gpio_config_t io_conf = {};
-//     // disable interrupt
-//     io_conf.intr_type = GPIO_INTR_DISABLE;
-//     // set as output mode
-//     io_conf.mode = GPIO_MODE_OUTPUT;
-//     // bit mask of the pins that you want to set,e.g.GPIO18/19
-//     io_conf.pin_bit_mask = LED_PIN_SEL;
-//     // disable pull-down mode
-//     io_conf.pull_down_en = 0;
-//     // disable pull-up mode
-//     io_conf.pull_up_en = 0;
-//     // configure GPIO with the given settings
-//     gpio_config(&io_conf);
-//     printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
-
-//     while (1)
-//     {
-//         gpio_set_level(LED_PIN, 1);
-//         vTaskDelay(1000 / portTICK_RATE_MS);
-//         gpio_set_level(LED_PIN, 0);
-//         vTaskDelay(1000 / portTICK_RATE_MS);
-//         printf("led.task\n");
-//     }
 }
--- user/user.h
+++ user/user.h
@@ -1,1 +1,14 @@
-#ifn
+#ifndef USER_INCLUDE_H
+#define USER_INCLUDE_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+
+void user_task(void *pvParameter);
+#endif
